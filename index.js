c8f03f10c39e076456f60f06d94988806b4d7942
Brian Dunnette <dunn0172@umn.edu>
2016-01-05 16:39:57 -0600

    returning tile_data; updating hapi

--- index.js
+++ index.js
@@ -40,8 +40,33 @@ server.route({
           });
         });
       }
-    })
+    });
+  }
+});
 
+server.route({
+  method: 'GET',
+  path: '/{tileFile}/{zoom}/{column}/{row}',
+  handler: function(request, reply) {
+    var tileFile = Path.join(__dirname, request.params.tileFile + '.wstiles');
+    FS.access(tileFile, FS.R_OK, function(err) {
+      if (err) {
+        reply('File not found').code(404);
+      } else {
+        var db = new Sqlite3.Database(Path.join(__dirname, request.params.tileFile + '.wstiles'), Sqlite3.OPEN_READONLY, function(error) {
+          var stmt = db.prepare("SELECT tile_data FROM tiles WHERE zoom_level=? AND tile_column=? AND tile_row=?");
+          stmt.get(request.params.zoom, request.params.column, request.params.row, function(err, row) {
+            if (err) {
+              reply(err).code(404);
+            } else if (row) {
+              reply(row.tile_data);
+            } else {
+              reply('Unknown error. Share and enjoy!').code(404);
+            }
+          });
+        });
+      }
+    });
   }
 });
 
