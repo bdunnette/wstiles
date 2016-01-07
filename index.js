a15477d09fb3af02848c65c7d612cdba8a0430e4
Brian Dunnette <dunn0172@umn.edu>
2016-01-07 16:57:30 -0600

    basic slide viewer

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Hoek = require('hoek');
 const Sqlite3 = require('sqlite3').verbose();
+const Glob = require("glob");
 const Path = require('path');
 const FS = require('fs');
 
 const server = new Hapi.Server();
+
 server.connection({
   port: 3000
+});
+
+server.register(require('inert'), (err) => {
+  if (err) {
+    throw err;
+  }
+});
+
+server.register(require('vision'), (err) => {
+  Hoek.assert(!err, err);
+
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: 'templates'
+  });
+});
+
+server.route({
+  method: 'GET',
+  path: '/public/{param*}',
+  handler: {
+    directory: {
+      path: 'public'
+    }
+  }
 });
 
 server.route({
   method: 'GET',
   path: '/',
   handler: function(request, reply) {
-    reply('Hello, world!');
+    Glob("*.wstiles", function(er, files) {
+      // files is an array of filenames.
+      // If the `nonull` option is set, and nothing
+      // was found, then files is ["**/*.js"]
+      // er is an error object or null.
+      var shortFiles = files.map(function(val) {
+        return val.replace('.wstiles', '')
+      });
+      reply({
+        slides: shortFiles
+      });
+    })
   }
 });
 
@@ -67,6 +109,27 @@ server.route({
         });
       }
     });
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/view',
+  handler: {
+    view: {
+      template: 'index',
+      context: {
+        title: 'Available Slides'
+      }
+    }
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/view/{tileFile}',
+  handler: function(request, reply) {
+    reply.view('slide', {tileFile: request.params.tileFile});
   }
 });
 
